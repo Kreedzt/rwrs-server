c7f92bf19c4367a932510c41d6bfc669d5a5a60d
Kreedzt <zhaozisong1@live.com>
2025-02-26 20:04:10 +0800

    :sparkles: add static folder serve

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use salvo::prelude::*;
 use salvo::proxy::{Proxy, ReqwestClient};
+use salvo::serve_static::StaticDir;
 use tracing::info;
 
 #[handler]
@@ -14,12 +15,11 @@ async fn main() {
     let router = Router::new()
         .hoop(RequestId::new())
         .push(Router::new().path("/ping").get(ping))
-        .push(Router::new().path("/server_list").goal(
-            Proxy::new(
-                vec!["http://rwr.runningwithrifles.com/rwr_server_list/get_server_list.php"],
-                ReqwestClient::default()
-            )
-        ));
+        .push(Router::new().path("/api/server_list").goal(Proxy::new(
+            vec!["http://rwr.runningwithrifles.com/rwr_server_list/get_server_list.php"],
+            ReqwestClient::default(),
+        )))
+        .push(Router::with_path("{**path}").get(StaticDir::new(["static"]).defaults("index.html")));
     let service = Service::new(router).hoop(Logger::new());
     let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
     Server::new(acceptor).serve(service).await;
